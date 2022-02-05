844d3d2f4668c8f7c803d20938aae310be674582
jlocke <jesse.locke.95@gmail.com>
2022-02-05 15:40:06 +0000

    add min and max collection size checks

--- src/main/kotlin/com/sagehadnt/validoo/Validoo.kt
+++ src/main/kotlin/com/sagehadnt/validoo/Validoo.kt
@@ -5,13 +5,27 @@ import kotlin.reflect.KProperty
 @DslMarker
 annotation class ValidooDSL
 
+/**
+ * Begin validation on the receiver. This function will perform all the checks specified in the lambda, returning
+ * a success object if all pass, or a failure object containing all errors otherwise.
+ */
 @ValidooDSL
-fun <T> T.validate(op: ValidationBuilder<T>.() -> Unit) = ValidationBuilder(this).apply(op).execute()
+fun <T> T.validate(op: ValidationBuilder<T>.() -> Unit): ValidationResult<T> = ValidationBuilder(this).apply(op).execute()
 
 class ValidationBuilder<T>(private val value: T) {
 
     private val failures = mutableSetOf<FailureReason<T>>()
 
+    /**
+     * Add a check to the validation. A check consists of a string description of the check, followed by a lambda
+     * to perform the check. When the validation executes, all the lambdas are run, and the string descriptions of
+     * all failing checks are reported.
+     *
+     * As this function overrides invoke(), you can access it using syntax like in the following example:
+     * ```
+     * "name must be at least three characters" { name.length >= 3 }
+     * ```
+     */
     @ValidooDSL
     operator fun String.invoke(condition: T.() -> Boolean) {
         if (!condition(value)) {
@@ -19,17 +33,42 @@ class ValidationBuilder<T>(private val value: T) {
         }
     }
 
+    /**
+     * Check all the members of a collection on the object under validation. If any of the elements fail their
+     * validation, the overall validation fails.
+     */
     @ValidooDSL
-    fun <A> validateCollection(prop: KProperty<Iterable<A>>, condition: A.() -> ValidationResult<A>) {
-        val nestedFailures = prop.getter.call(value)
+    fun <A> validateCollection(prop: KProperty<Iterable<A>>,
+                               /**
+                                * The minimum allowed size of the collection.
+                                */
+                               minInclusive: Int? = null,
+                               /**
+                                * The maximum allowed size of the collection.
+                                */
+                               maxInclusive: Int? = null,
+                               condition: ValidationBuilder<A>.() -> Unit) {
+        val propName = prop.name
+        val collection = prop.getter.call(value).toList()
+        minInclusive?.let {
+            if (collection.size < it) {
+                this.failures.add(collectionTooSmallFailure(propName, it))
+            }
+        }
+        maxInclusive?.let {
+            if (collection.size > it) {
+                this.failures.add(collectionTooBigFailure(propName, it))
+            }
+        }
+        val nestedFailures = collection
             .mapNotNull {
-                val result = condition(it)
+                val result: ValidationResult<A> = it.validate(condition)
                 if (result is ValidationResult.Failure<A>) {
                     FailureReason.Group.Element(it, result.reasons)
                 } else null
             }.toSet()
         if (nestedFailures.isNotEmpty()) {
-            this.failures.add(FailureReason.Group(prop.name, nestedFailures))
+            this.failures.add(FailureReason.Group(propName, nestedFailures))
         }
     }
 
@@ -39,6 +78,11 @@ class ValidationBuilder<T>(private val value: T) {
         } else {
             ValidationResult.Failure(value, failures)
         }
+    }
+
+    companion object {
+        fun <T> collectionTooSmallFailure(name: String, minInclusive: Int) = FailureReason.Simple<T>("$name must have at least $minInclusive elements")
+        fun <T> collectionTooBigFailure(name: String, maxInclusive: Int) = FailureReason.Simple<T>("$name must have no more than $maxInclusive elements")
     }
 }
 
@@ -55,10 +99,12 @@ sealed interface ValidationResult<T> {
 
     operator fun invoke(): T
     val reasons: Set<FailureReason<T>>
+    fun orNull(): T?
 
     class Success<T>(val result: T) : ValidationResult<T> {
         override fun invoke(): T = result
         override val reasons = emptySet<FailureReason<T>>()
+        override fun orNull(): T? = result
     }
 
     class Failure<T>(val source: T, override val reasons: Set<FailureReason<T>>) : ValidationResult<T> {
@@ -69,6 +115,7 @@ sealed interface ValidationResult<T> {
         }
 
         override fun invoke(): T = error(errorMsg)
+        override fun orNull(): T? = null
     }
 }
 
--- src/test/kotlin/com/sagehadnt/validoo/ValidooTest.kt
+++ src/test/kotlin/com/sagehadnt/validoo/ValidooTest.kt
@@ -32,6 +32,27 @@ class ValidooTest {
     }
 
     @Test
+    fun `validateCollection should fail if smaller than min size`() {
+        val result = Company("", emptySet()).validate {
+            validateCollection(Company::employees, minInclusive = 1) { /* no conditions */ }
+        }
+        assertThat(result).isInstanceOf(ValidationResult.Failure::class.java)
+        assertThat(result.reasons).containsExactly(ValidationBuilder.collectionTooSmallFailure("employees", 1))
+    }
+
+    @Test
+    fun `validateCollection should fail if larger than max size`() {
+        val result = Company("", setOf(
+            Employee("Alice", 40),
+            Employee("Bob", 30)
+        )).validate {
+            validateCollection(Company::employees, maxInclusive = 1) { /* no conditions */ }
+        }
+        assertThat(result).isInstanceOf(ValidationResult.Failure::class.java)
+        assertThat(result.reasons).containsExactly(ValidationBuilder.collectionTooBigFailure("employees", 1))
+    }
+
+    @Test
     fun `nested validation failure`() {
         val validEmployee = Employee("Alice", 40)
         val invalidEmployee = Employee("Bob", 5)
@@ -59,7 +80,10 @@ private const val COMPANY_NAME_REQUIREMENT = "name must be at least 1 character"
 fun performTestValidation(company: Company): ValidationResult<Company> = company.validate {
     COMPANY_HAS_EMPLOYEE_REQUIREMENT { employees.isNotEmpty() }
     COMPANY_NAME_REQUIREMENT { name.isNotEmpty() }
-    validateCollection(Company::employees) { performTestValidation(this) }
+    validateCollection(Company::employees) {
+        EMPLOYEE_AGE_REQUIREMENT { age >= 18 }
+        EMPLOYEE_NAME_REQUIREMENT { name.isNotEmpty() }
+    }
 }
 
 private const val EMPLOYEE_AGE_REQUIREMENT = "should be 18 or older"
